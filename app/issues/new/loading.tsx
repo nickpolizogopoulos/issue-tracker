b03ef407117bf12f6b012458d52f1350d006f8e1
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2025-01-27 16:57:46 +0200

    Refactor: Organise import statements

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,14 +1,13 @@
+import { Skeleton } from '@/app/components';
 import { Box } from '@radix-ui/themes';
-import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css';
 import BackToIssues from '../BackToIssues';
 
 const LoadingNewIssuePage = () => {
   return (
     <Box className='max-w-xl'>
       <BackToIssues />
-      <Skeleton />
-      <Skeleton height='2rem' />
+      <Skeleton width='7rem' height='1.2rem' />
+      <Skeleton height='1.5rem' />
       <Skeleton height='20rem' />
     </Box>
   );
