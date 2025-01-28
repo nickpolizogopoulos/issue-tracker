5f2d8e6e4fa698631e0bc1b0ee542fdb3142cb6c
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2025-01-28 21:32:54 +0200

    Fix caching issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -61,6 +61,7 @@ const IssueForm = ( {issue}: Props ) => {
         await axios.post('/api/issues', data);
 
       router.push('/issues');
+      router.refresh();
     } 
     catch (error) {
       setSubmitting(false);
