f58aa4ab5e3d886397249c083383282974954b99
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2025-01-29 14:04:50 +0200

    Refactor NavBar layout to use Container and Flex for improved structure and styling

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Box } from "@radix-ui/themes";
+import { Box, Container, Flex } from "@radix-ui/themes";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -28,35 +28,41 @@ const NavBar = () => {
     const currentPath = usePathname();
 
     return (
-        <nav className="flex space-x-10 border-b mb-5 px-5 h-14 items-center select-none">
-            <Link href="/">
-                <img src="/logo.svg" className="w-10" alt="Issue Tracker Logo" />
-            </Link>
-            <ul className="flex space-x-5">
-                    {
-                        links.map( link => 
-                            <li key={link.href}>
-                                <Link 
-                                className={`
-                                    ${
-                                        link.href === currentPath 
-                                        ? 'underline decoration-2 underline-offset-4 text-zinc-700' 
-                                        : 'text-zinc-500'
-                                    } 
-                                    hover:text-zinc-700 hover:underline decoration-2 underline-offset-4 transition-colors`
+        <nav className="border-b mb-5 px-5 py-3 select-none">
+            <Container>
+                <Flex justify='between' align='center'>
+                    <Flex align='center' gap='5'>
+                        <Link href="/">
+                            <img src="/logo.svg" className="w-10" alt="Issue Tracker Logo" />
+                        </Link>
+                        <ul className="flex space-x-5">
+                                {
+                                    links.map( link => 
+                                        <li key={link.href}>
+                                            <Link 
+                                            className={`
+                                                ${
+                                                    link.href === currentPath 
+                                                    ? 'underline decoration-2 underline-offset-4 text-zinc-700' 
+                                                    : 'text-zinc-500'
+                                                } 
+                                                hover:text-zinc-700 hover:underline decoration-2 underline-offset-4 transition-colors`
+                                            }
+                                            href={link.href}
+                                            >
+                                                {link.label}
+                                            </Link>
+                                        </li>
+                                    )
                                 }
-                                href={link.href}
-                                >
-                                    {link.label}
-                                </Link>
-                            </li>
-                        )
-                    }
-            </ul>
-            <Box>
-                {status === 'authenticated' && <Link href='/api/auth/signout'>Logout</Link>}
-                {status === 'unauthenticated' && <Link href='/api/auth/signin'>Login</Link>}
-            </Box>
+                        </ul>
+                    </Flex>
+                    <Box>
+                        {status === 'authenticated' && <Link href='/api/auth/signout'>Logout</Link>}
+                        {status === 'unauthenticated' && <Link href='/api/auth/signin'>Login</Link>}
+                    </Box>
+                </Flex>
+            </Container>
         </nav>
     );
 };
