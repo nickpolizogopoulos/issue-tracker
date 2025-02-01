1200503119ffc9b95ba086e8abaed0c7fc8fbfa7
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2025-02-01 14:09:30 +0200

    Assign an issue to a user

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,45 +1,64 @@
 'use client';
 
 import { Skeleton } from '@/app/components';
-import { User } from "@prisma/client";
-import { Select } from "@radix-ui/themes";
+import { Issue, User } from "@prisma/client";
+import { Select, Separator } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { useState } from 'react';
 
-const AssigneeSelect = () => {
+type Props = {
+    issue: Issue;
+};
 
-    const {data: users, error, isLoading} = useQuery<User[]>({
+const AssigneeSelect = ({ issue }: Props) => {
+
+    const { data: users, error, isLoading } = useQuery<User[]>({
         queryKey: ['users'],
-        queryFn: () => 
-            axios
-                .get('/api/users')
-                .then( response => response.data ),
-        staleTime: 60 * 1000, //1 min
+        queryFn: () => axios
+            .get('/api/users')
+            .then(res => res.data),
+        staleTime: 60 * 1000, // 1 min
         retry: 3,
     });
 
-    if (isLoading)
+    const [value, setValue] = useState<string | null>(issue.assignedToUserId ?? "null");
+
+    const handleChange = (userId: string) => {
+        const newValue = userId === "null" ? null : userId;
+        setValue(newValue);
+
+        try {
+            axios.patch(`/api/issues/${issue.id}`, { assignedToUserId: newValue });
+        }
+        catch (error) {
+            console.error("Failed to update assignee", error);
+        }
+    };
+
+    if (isLoading) 
         return <Skeleton height='1.75rem' />;
 
     if (error)
         return null;
 
     return (
-        <Select.Root>
+        <Select.Root defaultValue={value || 'null'} onValueChange={handleChange}>
             <Select.Trigger placeholder='Assign...' />
             <Select.Content>
                 <Select.Group>
                     <Select.Label>Suggestions</Select.Label>
+                    <Separator size='4' />
+                    <Select.Item value='null'>Unassigned</Select.Item>
                     {
-                        users?.map( user => (
+                        users?.map(user => 
                             <Select.Item 
                                 key={user.id}
                                 value={user.id}
                             >
                                 {user.name}
                             </Select.Item>
-                        ))
-
+                        )
                     }
                 </Select.Group>
             </Select.Content>
